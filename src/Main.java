8e7544a26224e8d0fcf3a116e1cd43f755a89cb8
Kateryna2882 <govoryhina82@gmail.com>
2024-03-15 19:30:43 +0200

    add

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        while loop = executes a block of code as a long as a it's condition remains true
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+        while (name.isBlank()){
+            System.out.println("Enter your name: ");
+            name = scanner.nextLine();
+        }
+        System.out.println("Helo " + name);
     }
 }
