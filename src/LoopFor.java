38f376e7997ce4512a31a889ba8fd6de93565730
Kateryna2882 <govoryhina82@gmail.com>
2024-03-15 19:53:27 +0200

    add

---
+++ src/LoopFor.java
@@ -0,0 +1,14 @@
+public class LoopFor {
+    public static void main(String[] args) {
+//        for loop = executes a block of code a limited amount of times
+//        for (int i = 0; i <= 10; i++) {
+        for (int k = 10; k >= 0; k-=2) {
+//            for (int j = 10; j >= 0; j--) {
+//            }
+//                System.out.println(i);
+//                System.out.println(j);
+                System.out.println(k);
+            }
+            System.out.println("Happy new year");
+        }
+    }
